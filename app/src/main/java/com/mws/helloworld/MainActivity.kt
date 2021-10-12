bfb51172d144e8eb6ba14c6c1af0ba9353a193d3
_ardy <ardy.erdiyanto@gmail.com>
2021-10-12 10:02:03 +0700

    aplikasi penghitung volume

--- app/src/main/java/com/mws/helloworld/MainActivity.kt
+++ app/src/main/java/com/mws/helloworld/MainActivity.kt
@@ -2,17 +2,42 @@ package com.mws.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var txtPanjang : EditText
+    private lateinit var txtLebar : EditText
+    private lateinit var txtTinggi : EditText
+    private lateinit var btnHitung : Button
+    private lateinit var txtResult : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // ini variabel orang
-        var namaLengkap : String = "budi"
-        var usia = 17
+        txtPanjang = findViewById(R.id.txtPanjang)
+        txtLebar = findViewById(R.id.txtLebar)
+        txtTinggi = findViewById(R.id.txtTinggi)
+        btnHitung = findViewById(R.id.btnHitung)
+        txtResult = findViewById(R.id.txtResult)
 
-        // ini variabel khusus ....
-        var status : Boolean = true
+        btnHitung.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View) {
+        if(p0.id == R.id.btnHitung) {
+            val inputPanjang = txtPanjang.text.toString().trim()
+            val inputLebar = txtLebar.text.toString().trim()
+            val inputTinggi = txtTinggi.text.toString().trim()
+
+            val volume = inputPanjang.toDouble() * inputLebar.toDouble() * inputTinggi.toDouble()
+
+            txtResult.text = volume.toString()
+        }
     }
 }
+
